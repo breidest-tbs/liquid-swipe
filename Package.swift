2e65657188131ba12b4ebc068e562c251cc5d974
breidest-tbs <stephan.breidenbach@solingen.de>
2021-05-21 17:53:43 +0200

    Package.swift manifest file added

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "liquid-swipe",
-            path: "Example",
+            path: "liquid-swipe/Classes",
 	    exclude: ["Podfile"]),
         .testTarget(
             name: "liquid-swipeTests",
