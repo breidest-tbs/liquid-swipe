506d11802106f1468cd65f288e55e74fb3370310
breidest-tbs <stephan.breidenbach@solingen.de>
2021-05-21 16:36:24 +0200

    Package.swift manifest file added

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "liquid-swipe",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "liquid-swipe",
+            targets: ["liquid-swipe"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/facebook/pop.git", from: "1.0.12"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "liquid-swipe",
+            dependencies: []),
+        .testTarget(
+            name: "liquid-swipeTests",
+            dependencies: ["liquid-swipe"]),
+    ]
+)
+
